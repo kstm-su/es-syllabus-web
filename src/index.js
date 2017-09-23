a4765b0cfcef021c61e8b1d01f999152867e5669
bgpat <bgpat23@gmail.com>
2017-09-24 06:45:50 +0900

    Add themes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueMaterial from 'vue-material';
+import VueThemes from './themes';
 import App from './app';
 import routes from './routes';
 
@@ -11,6 +12,7 @@ import 'vue-material/dist/vue-material.css';
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 Vue.use(VueMaterial);
+Vue.material.registerTheme(VueThemes);
 
 new Vue({
   el: '#app',
